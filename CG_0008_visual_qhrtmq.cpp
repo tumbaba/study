63a418b324dbc1dca3b41cfc789c8897a4d7b396
Changgeuns <ckdrms8110@gmail.com>
2024-05-21 20:02:07 +0900

    구구단

---
+++ CG_0008_visual_qhrtmq.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+    //// 값 입력받아서 원하는 단만 출력하기
+    int num1 = 0;
+
+    cin >> num1;
+
+    for (int a = 1; a <= 9; a++)
+    {
+        cout << num1 << "x" << a << "=" << num1 * a << endl;
+    }
+
+
+
+}
+
