0733d97b80626d4dc518561d7b97af57e99c2b4b
tumbaba <168721037+tumbaba@users.noreply.github.com>
2024-05-21 20:42:58 +0900

    ME_96  구구단

---
+++ FileName.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int num;
+
+	cin >> num;
+
+    for (int i = 1; i < 10; i++)
+	{
+		cout << num << " * " << i << " = "<< i * num << endl;
+	}
+
+
+	return 0;
+}
