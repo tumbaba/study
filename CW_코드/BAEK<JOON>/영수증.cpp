9c743f204a2b2ff902602af10143c46bf10c25ff
iristopvt <iristopvt@naver.com>
2024-05-24 21:53:11 +0900

    영수증

---
+++ CW_코드/BAEK<JOON>/영수증.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int total; //합계금액
+	cin >> total; 
+
+	int number; //물건 종류의 수
+	cin >> number; 
+
+	int sum = 0; 
+
+	int price, quantity; //물건 가격 수량 
+
+	for (int i = 0; i < number; i++)
+	{
+		
+		cin >> price >> quantity;
+		sum += (price * quantity);
+	}
+
+	if (sum == total)
+	{
+		cout << "Yes" << endl;
+	}
+	else
+	{
+		cout << "No" << endl;
+	}
+	return 0;
+}
