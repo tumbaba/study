80236d5fe795a7b42f69e9f1c42db50f8ed2d9cf
iristopvt <iristopvt@naver.com>
2024-05-24 21:51:43 +0900

    BAEK<JOON>_구구단

---
+++ CW_코드/BAEK<JOON>/구구단.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    
+    cin >> N;
+
+    for (int i = 1; i <= 9; i++)
+    {
+        cout << N << " * " << i << " = " << N * i << endl;
+    }
+    return 0;
+}
