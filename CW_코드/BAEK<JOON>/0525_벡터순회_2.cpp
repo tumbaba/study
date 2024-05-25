d6246e1eb53f051f8287bf7181ffa5a2c6e9dd17
iristopvt <iristopvt@naver.com>
2024-05-25 15:02:18 +0900

    문제2_벡터순회

---
+++ CW_코드/BAEK<JOON>/0525_벡터순회_2.cpp
@@ -0,0 +1,19 @@
+﻿// 0525_배열순회_2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<string> names = { "Alice", "Bob", "Charlie" };
+
+	for (string name : names)
+	{
+		cout << name << "\n";
+	}
+
+}
+
