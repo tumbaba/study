fce6f329391b5b951ad7c24cf0d24f8a9a09025c
iristopvt <iristopvt@naver.com>
2024-05-25 15:01:32 +0900

    문제1_배열순회

---
+++ CW_코드/BAEK<JOON>/0525_배열순회.cpp
@@ -0,0 +1,18 @@
+﻿// 0525_배열순회.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int numbers[] = { 10, 20, 30, 40, 50 };
+
+    for (int a : numbers)
+    {
+        cout << a << " ";
+    }
+   
+}
