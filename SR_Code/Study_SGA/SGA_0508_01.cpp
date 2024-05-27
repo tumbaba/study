2c0ac94fcbc0c22f54fef5a35bbb4ad99947db0f
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-05-27 18:01:39 +0900

    20240508_class_오버라이딩

---
+++ SR_Code/Study_SGA/SGA_0508_01.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "Animal.h"
+#include "Cat.h"
+#include "Dog.h"
+#include "Birld.h"
+
+
+
+int main() {
+
+	Animal animal;
+	
+	Cat cat;
+	Birld birld;
+
+	birld.playSound(); // overrid (오버라이드) 된 자식 playsound 호출
+
+	
+	
+	// 파일 입출력
+
+
+
+
+
+
+
+	
+	
+
+
+	return 0;
+}
