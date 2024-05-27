b0141b8052e0b07d859b1f744ec3240b88b40a31
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-05-27 16:46:44 +0900

    20240419_포인트&Swap

---
+++ SR_Code/Study_SGA/ 20240419/main.h
@@ -0,0 +1,5 @@
+#pragma once                  // 중복된 선언을 막아주는 기본적으로 포함되어있음 (예: iostream 같은)
+
+#define _CRT_SECURE_NO_WARNINGS  //  안전하지않다는 원인 컴파일내용들이 뜨지않음           
+#include <iostream>  // 기본 c++에서 사용하는 라이브러리  
+using namespace std;
