df71e200236b695d9dab073c23be43776f4ee2b9
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-05-27 17:01:36 +0900

    20240422_함수(헤더, cpp, 포인트, enum)

---
+++ SR_Code/Study_SGA/20240422/Func.h
@@ -0,0 +1,5 @@
+#pragma once // 윈도우에서만 사용되는 문구
+
+int sum(int x); //─────────── 함수 선언
+int myprint(int x);//─────────── 함수 선언
+
--- SR_Code/Study_SGA/Func.h
+++
@@ -1,5 +0,0 @@
-#pragma once // 윈도우에서만 사용되는 문구
-
-int sum(int x); //─────────── 함수 선언
-int myprint(int x);//─────────── 함수 선언
-
