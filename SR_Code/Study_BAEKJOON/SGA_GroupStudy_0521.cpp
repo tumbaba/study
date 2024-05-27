511e30361b5cac2db107f064f0084ab385867d3f
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-05-27 19:42:54 +0900

    구구단

--- SR_Code/Study_BAEKJOON/SGA_GroupStudy_0521.cpp
+++ SR_Code/Study_BAEKJOON/SGA_GroupStudy_0521.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-// 구구단
+// 구구단  
 //N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
 
 int main() {
@@ -12,8 +12,6 @@ int main() {
 	{
 		std::cout << N << " * " << a << " = " << N * a << std::endl;
 	}
-
-
 
 	return 0;
 }
