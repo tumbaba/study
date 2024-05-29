8971ccec9acb272397c9bda6e3828c4c4985e03a
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-05-29 14:35:35 +0900

    WINDOW_API(Class, 헤더 구성)

---
+++ SR_Code/Study_SGA/20240529/SGA_0529_02.cpp
@@ -0,0 +1,13 @@
+#include "WindowApp.h"
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	WindowApp app(hInstance);
+
+	if (!app.Initalize())
+	{
+		return 1;
+	}
+
+	return app.Run();
+}
