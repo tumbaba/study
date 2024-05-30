e3e57a1ef3bbcc31f426f88a54c1df798d0e1333
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-05-31 08:53:35 +0900

    WinAPI-기본만들기

---
+++ SR_Code/Study_SGA/20240530/WindowApp.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <Windows.h>
+
+
+class WindowApp
+{
+public:
+	WindowApp(HINSTANCE hInstance);
+	~WindowApp();
+
+	bool Initalize(int width, int height);
+	//bool Initalize();
+	int Run();
+	int g_App();
+
+private:
+	HINSTANCE hInstance;
+	HWND hwnd;
+
+	static WindowApp* g_pApp;
+
+	LPCWSTR classname = L"WindowClass";
+
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	LRESULT HandleMessage(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	bool RegisterWindowClass();
+	bool CreateAppWindow(int nCmdShow, int width, int height);
+
+
+	//마우스 상태의 변수
+	bool isDrawing = false;
+	POINT startPoint, endPoint;
+
+};
+
